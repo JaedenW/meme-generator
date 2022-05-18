523e29384231abc035a85aaef96df7231f37352e
Jaeden <jaedenjwest@gmail.com>
2022-05-18 21:04:26 +1200

    Added dynamic meme image

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme() {
+	const [memeImage, setMemeImage] = React.useState('');
+
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const url =
+			memesArray[Math.floor(Math.random() * memesArray.length)].url;
+		setMemeImage(url);
+	}
+	function handleOnMouseOver() {}
+
 	return (
 		<main>
-			<form className="form">
+			<div className="form">
 				<input
 					className="form--input"
 					type="text"
@@ -14,8 +25,15 @@ export default function Meme() {
 					type="text"
 					placeholder="Bottom Text"
 				></input>
-				<button className="form--button">Get a New Meme Image 🖼</button>
-			</form>
+				<button
+					className="form--button"
+					onClick={getMemeImage}
+					onMouseOver={handleOnMouseOver}
+				>
+					Get a New Meme Image 🖼
+				</button>
+				<img className="meme--image" src={memeImage} alt="Meme" />
+			</div>
 		</main>
 	);
 }
